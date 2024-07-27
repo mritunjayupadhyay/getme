73d36f8b26d93c87c38318c3716ec9df75127fa3
mritunjayupadhyay <mupadhyay00@gmail.com>
2024-07-27 12:42:50 +0530

    feat: import body parser

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import bodyParser from "body-parser";
 import 'dotenv/config';
 import meRouter from "./routes/get_me.routes.js";
 import uploadRouter from "./routes/upload.routes.js";
